643159bf217b55686eb38dccf95b1be7410ac12b
LuCieBeGie <105434870+LuCieBeGie@users.noreply.github.com>
2023-09-05 17:50:03 +0200

    Update script.js

--- script/script.js
+++ script/script.js
@@ -9,11 +9,16 @@ function sendEmail(email) {
     emailjs.send(serviceId, templateId, params)
         .then(
             res => {
-                console.log(document.querySelector('input[name="answer"]:checked').value);
                 document.querySelector('input[name="answer"]:checked').value = ''
+                clearRadio()
                 document.getElementById('suggestion').value = ''
                 console.log(res);
                 alert('Success')
             }
         ).catch(err => console.log(err));
+}
+
+function clearRadio() {
+    let radio = document.getElementById("answer");
+    radio.checked = false;
 }
