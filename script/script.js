8ad2210322947aa4c9a6640d26186a2732736321
LuCieBeGie <105434870+LuCieBeGie@users.noreply.github.com>
2023-09-07 09:56:02 +0200

    update

--- script/script.js
+++ script/script.js
@@ -1,24 +1,36 @@
-function sendEmail(email) {
+const submit = document.querySelector('#submit');
+
+function sendEmail() {
     let params = {
         answer: document.querySelector('input[name="answer"]:checked').value,
         suggestion: document.getElementById('suggestion').value
     }
     const serviceId = 'service_jkmmnpu'
     const templateId = 'template_cvdxllr'
-
     emailjs.send(serviceId, templateId, params)
         .then(
             res => {
-                document.querySelector('input[name="answer"]:checked').value = ''
                 clearRadio()
                 document.getElementById('suggestion').value = ''
-                console.log(res);
                 alert('Success')
             }
         ).catch(err => console.log(err));
 }
 
 function clearRadio() {
-    let radio = document.getElementById("answer");
+    let radio = document.querySelector('input[name="answer"]:checked');
     radio.checked = false;
 }
+
+function sendForm() {
+    submit.addEventListener('click', function () {
+        if (document.querySelector('input[name="answer"]:checked').value == 'NO') {
+            sendEmail()
+        } else {
+            location.assign('./registration/registration.html')
+            console.log(false);
+        }
+    })
+}
+
+sendForm()
